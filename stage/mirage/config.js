97b0613202ae104627ebf82b901e9e1173d91326
James Neilson <james@quandl.com>
2018-09-21 13:31:08 -0700

    Not working yet :(

--- stage/mirage/config.js
+++ stage/mirage/config.js
@@ -1,13 +1,7 @@
 export default function() {
   this.urlPrefix = 'http://localhost:3000';
 
-  this.get('/campaigns', () => {
-    return {
-      campaigns: [
-        { title: 'Wheel of Time', description: 'Damn good' },
-        { title: 'Snowcrash', description: 'Also damn good' },
-        { title: 'Neuromancer', description: 'Wowee' }
-      ]
-    }
+  this.get('/campaigns', (schema) => {
+    return schema.campaigns.all();
   });
 }
