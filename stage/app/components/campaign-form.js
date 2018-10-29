097cfad9d8ddbae1484faaa932dd03387b7f3ee3
James Neilson <james@quandl.com>
2018-10-29 19:13:42 -0400

    Tons of random changes. Most don't work yet

--- stage/app/components/campaign-form.js
+++ stage/app/components/campaign-form.js
@@ -2,7 +2,19 @@ import Component from '@ember/component';
 import { inject as service } from '@ember/service';
 
 export default Component.extend({
-  currentUser: service('current-user'),
+  session: service(),
+  currentUser: service(),
+  campaign: null,
+
+  didInsertElement() {
+  },
+
+  _resetCampaign() {
+    this.set('campaign', this.get('store').createRecord('campaign', {
+      user: this.get('referrer'),
+      key: this.get('key')
+    }));
+  },
 
   actions: {
     async createCampaign() {
--- stage/app/controllers/login.js
+++ stage/app/controllers/login.js
@@ -7,7 +7,9 @@ export default Controller.extend({
   actions: {
     async authenticate() {
       let { identification, password } = this.getProperties('identification', 'password');
-      this.get('session').authenticate('authenticator:oauth2', identification, password).catch((reason) => {
+      this.get('session').authenticate('authenticator:oauth2', identification, password).then((response) => {
+        this.session.data.set('user_id', response.user_id);
+      }).catch((reason) => {
         this.set('errorMessage', reason.error || reason);
       });
       this.transitionToRoute('/');
