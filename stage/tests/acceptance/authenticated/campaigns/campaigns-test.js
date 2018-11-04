6e2a370f689539636d91453a7b271a1b5e0af257
James Neilson <james@quandl.com>
2018-11-03 23:47:36 -0400

    Write helper function for easier testing

--- stage/tests/acceptance/authenticated/campaigns/campaigns-test.js
+++ stage/tests/acceptance/authenticated/campaigns/campaigns-test.js
@@ -2,14 +2,8 @@ import { module, test } from 'qunit';
 import { setupApplicationTest } from 'ember-qunit';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 import { visit } from '@ember/test-helpers';
-import Service from '@ember/service';
 import { authenticateSession } from 'ember-simple-auth/test-support';
-
-let StubCurrentUserService = Service.extend({
-  load() {
-    return server.schema.users.all()[0];
-  }
-});
+import { setCurrentUser } from 'stage/tests/helpers/sessions/set-current-user';
 
 let campaign, user;
 
@@ -18,8 +12,7 @@ module('Acceptance | Authenticated | Campaigns', function(hooks) {
   setupMirage(hooks);
 
   hooks.beforeEach(async function() {
-    this.owner.register('service:current-user', StubCurrentUserService);
-    user = server.create('user');
+    user = setCurrentUser(this);
     await authenticateSession({
       identification: user.email,
       password: user.password
