6834b779f23cb0624fcad6ce65fcce93d21f26ef
James Neilson <james@quandl.com>
2018-11-04 12:10:57 -0500

    Fix bug, start writing test for new users route

--- stage/mirage/config.js
+++ stage/mirage/config.js
@@ -3,12 +3,16 @@ export default function() {
   this.get('/me', function(db) {
     return db.users.all()[0];
   });
-  this.post('/oauth/token', function(db) {
-    return db.users.all()[0];
-  });
   this.post('/users', function(db, request) {
     var attrs = JSON.parse(request.requestBody).data.attributes;
-    var user = db.users.create(attrs);
-    return user;
+    return db.users.create(attrs);
+  });
+
+  this.post('http://localhost:3000/oauth/token', function() {
+    return {
+      'access_token': 'acbde',
+      'token_type': 'Bearer',
+      'expires_in': 7200,
+      'created_at': 68354867865}
   });
 }
--- stage/tests/acceptance/users/new-test.js
+++ stage/tests/acceptance/users/new-test.js
@@ -1,7 +1,8 @@
-import { module, skip } from 'qunit';
+import { module, test } from 'qunit';
 import { setupApplicationTest } from 'ember-qunit';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 import { visit, currentURL, fillIn, click } from '@ember/test-helpers';
+import { authenticateSession } from 'ember-simple-auth/test-support';
 
 module('Acceptance | Users | New', function(hooks) {
   setupApplicationTest(hooks);
@@ -11,15 +12,21 @@ module('Acceptance | Users | New', function(hooks) {
     await visit('/users/sign_up');
   })
 
-  skip('can navigate to sign up form', async function(assert) {
+  test('can navigate to sign up form', async function(assert) {
     assert.equal(currentURL(), '/users/sign_up')
   });
 
-  skip('can sign up', async function(assert) {
+  test('submitting form allows user to sign up', async function(assert) {
+    let user = server.build('user');
+    this.set('model', user);
     await fillIn('[data-test-form="username"]', 'guybrush');
     await fillIn('[data-test-form="email"]', 'guy@threepwood.com');
     await fillIn('[data-test-form="password"]', 'sosecure');
     await fillIn('[data-test-form="password-confirmation"]', 'sosecure');
+    await authenticateSession({
+      identification: user.email,
+      password: user.password
+    });
     await click('[data-test-form="submit"]');
 
     assert.equal(currentURL(), '/campaigns')
