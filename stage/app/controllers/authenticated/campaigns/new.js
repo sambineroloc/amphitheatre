93294d3efdd3dae6b8ac505c3e468fb057fdd6ad
James Neilson <james@quandl.com>
2018-11-08 22:36:19 -0500

    User is set automatically and campaigns can be created

--- stage/app/controllers/authenticated/campaigns/new.js
+++ stage/app/controllers/authenticated/campaigns/new.js
@@ -3,18 +3,12 @@ import { inject as service } from '@ember/service';
 
 export default Controller.extend({
   session: service(),
-  currentUser: service('current-user'),
 
   actions: {
     async createCampaign() {
-      // let user = this.get('model');
-      // user.save().then(() => {
-      //   let identification = user.get('email');
-      //   let password = user.get('password');
-      //   this.get('session').authenticate('authenticator:oauth2', identification, password).catch((reason) => {
-      //     this.set('errorMessage', reason.error || reason);
-      //   });
-      // });
+      this.get('model').save().then(() => {
+        this.transitionToRoute('/');
+      });
     }
   }
 });
